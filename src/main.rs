234e212a8240013604851cbd0b5af5049d7fdc69
yantene <contact@yantene.net>
2023-01-28 07:15:48 +0000

    feat(app): add natural language generation task example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use rust_bert::pipelines::generation_utils::LanguageGenerator;
+use rust_bert::{gpt2::GPT2Generator, pipelines::generation_utils::GenerateOptions};
+
 fn main() {
-    println!("Hello, world!");
+    let model = GPT2Generator::new(Default::default()).unwrap();
+
+    let input_context_1 = "The dog";
+    let input_context_2 = "The cat was";
+
+    let generate_options = GenerateOptions {
+        max_length: Some(30),
+        ..Default::default()
+    };
+
+    let output = model.generate(
+        Some(&[input_context_1, input_context_2]),
+        Some(generate_options),
+    );
+
+    for item in output {
+        println!("{}", item.text)
+    }
 }
