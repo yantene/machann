416615cbf3bedb21ebcc429edb91ffead6c362ae
yantene <contact@yantene.net>
2023-01-31 00:06:38 +0000

    feat: use japanese-gpt-1b model

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,73 @@
-use rust_bert::pipelines::generation_utils::LanguageGenerator;
-use rust_bert::{gpt2::GPT2Generator, pipelines::generation_utils::GenerateOptions};
+use rust_bert::gpt2::GPT2Generator;
+use rust_bert::pipelines::common::{ModelType, TokenizerOption};
+use rust_bert::pipelines::generation_utils::{GenerateConfig, LanguageGenerator};
+use rust_bert::resources::LocalResource;
+use std::io::{self, stdout, Write};
+use std::path::PathBuf;
+
+fn ask(query: &str, gpt2_model: &GPT2Generator) -> String {
+    let preprocessed_query = query
+        .replace('\n', "")
+        .replace('「', "『")
+        .replace('」', "』");
+
+    let input_text = format!("わたし「{preprocessed_query}」マサル「");
+
+    let generated = gpt2_model.generate(Some(&[&input_text]), None);
+
+    let output_text = &generated[0].text;
+
+    output_text
+        .replace(['「', '」'], "\0")
+        .split('\0')
+        .nth(3)
+        .unwrap()
+        .to_string()
+}
 
 fn main() {
-    let model = GPT2Generator::new(Default::default()).unwrap();
+    let model_path = "./models/japanese-gpt-1b/rust_model.ot";
+    let model_resource = Box::new(LocalResource {
+        local_path: PathBuf::from(model_path),
+    });
 
-    let input_context_1 = "The dog";
-    let input_context_2 = "The cat was";
+    let config_path = "./models/japanese-gpt-1b/config.json";
+    let config_resource = Box::new(LocalResource {
+        local_path: PathBuf::from(config_path),
+    });
 
-    let generate_options = GenerateOptions {
-        max_length: Some(30),
+    let vocab_path = "./models/japanese-gpt-1b/spiece.model";
+    let vocab_resource = Box::new(LocalResource {
+        // Open spiece.model in a binary editor and replace the data as follows.
+        // [CLS] -> <cls>, [UNK] -> <unk>, [PAD] -> <pad>, [SEP] -> <sep>, [MASK] -> <mask>
+        local_path: PathBuf::from(vocab_path),
+    });
+    let merges_resource = vocab_resource.clone();
+
+    let generate_config = GenerateConfig {
+        model_resource,
+        config_resource,
+        vocab_resource,
+        merges_resource: Some(merges_resource),
+        top_k: 500,
         ..Default::default()
     };
 
-    let output = model.generate(
-        Some(&[input_context_1, input_context_2]),
-        Some(generate_options),
-    );
+    let tokenizer_option =
+        TokenizerOption::from_file(ModelType::T5, vocab_path, None, false, None, None).unwrap();
 
-    for item in output {
-        println!("{}", item.text)
+    let gpt2_model = GPT2Generator::new_with_tokenizer(generate_config, tokenizer_option).unwrap();
+
+    let mut question = String::new();
+
+    loop {
+        print!("You: ");
+        stdout().flush().unwrap();
+        io::stdin().read_line(&mut question).unwrap();
+
+        let answer = ask(&question, &gpt2_model);
+
+        println!("Machann: {answer}");
+        stdout().flush().unwrap();
     }
 }
